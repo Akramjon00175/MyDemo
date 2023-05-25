82e30cf79b18f277dfda3df3a76bfc04da694cc6
Akramjon <akramjon00175@gmail.com>
2023-05-25 11:28:20 -0700

    Initial Commit

---
+++ MyDemo/MyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoApp.swift
+//  MyDemo
+//
+//  Created by Akramjon on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
