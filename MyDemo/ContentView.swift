1ee1e53500a803870e58522ea419148ae885e49a
Akramjon <akramjon00175@gmail.com>
2023-05-30 10:37:30 -0700

    add item

--- MyDemo/ContentView.swift
+++ MyDemo/ContentView.swift
@@ -9,16 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        NavigationView {
+            Text("Akramjon Muhammadov")
+                .toolbar {
+                    ToolbarItem(placement: .principal){
+                        Text("Home")
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        
+                        Button(action: {
+                            
+                        }){
+                            Image(systemName: "apple.logo")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarLeading){
+                        Button(action: {
+                            
+                        }){
+                            Image(systemName: "line.3.horizontal")
+                                .foregroundColor(.black)
+                        }
+                    }
+                }
+            
         }
-        .padding()
+        
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
